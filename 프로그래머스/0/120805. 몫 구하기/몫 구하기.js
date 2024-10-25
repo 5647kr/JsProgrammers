133cda979eaa1e10dc8305de318e4b1cb1eff86b
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-10-25 22:40:48 +0900

    [level 0] Title: 몫 구하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    // 방법1
+    var answer = 0;
+    answer = Math.floor(num1 / num2);
+    return answer;
+    
+    // 방법2
+    // return Math.floor(num1 / num2);
+    
+    // 방법3
+    // solution = (num1, num2) => {return Math.floor(num1 / num2)};
+}
