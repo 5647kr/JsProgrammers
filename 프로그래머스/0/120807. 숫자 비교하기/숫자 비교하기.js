1055b62192fcd3aeac299ea9287f544714950e32
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-10-25 22:22:49 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,17 @@
+function solution(num1, num2) {
+    // 방법1
+    var answer = 0;
+    answer = (num1 === num2) ? 1 : -1;
+    return answer;
+    
+    // 방법2
+    // return (num1 === num2) ? 1 : -1;
+    
+    // 방법3
+    // if(num1 === num2) {
+    //   answer = 1;
+    // } else {
+    //   answer = -1
+    // }
+    // return answer;
+}
