d122274789d10126fd090acdd889202f88ecf892
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-10-25 22:18:35 +0900

    [level 0] Title: 두 수의 차, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
@@ -10,4 +10,7 @@ function solution(num1, num2) {
     // 방법3
     // var answer = num1 - num2;
     // return answer
+    
+    // 방법4(동료풀이)
+    // const solution = (num1, num2) => num1 - num2  
 }
