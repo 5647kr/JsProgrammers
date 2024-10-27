6634e363f7ed62ac9b0eddc70bdb6dc129177349
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-10-27 19:55:07 +0900

    [level 0] Title: 두 수의 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    // 방법1
+    var answer = 0;
+    answer = num1 + num2;
+    return answer;
+    
+    // 방법2
+    // return num1 + num2;
+    
+    // 방법3
+    // solution = (num1, num2) => num1 + num2
+}
