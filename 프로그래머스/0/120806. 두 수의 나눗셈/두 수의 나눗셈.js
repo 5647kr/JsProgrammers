b8ac776a2db20361e7fddb781f1426ea874a5167
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-10-27 20:03:26 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -9,4 +9,13 @@ function solution(num1, num2) {
     
     // 방법3
     // solution = (num1, num2) => parseInt((num1 / num2) * 1000);
+    
+    // 방법4(동료풀이)
+    // return ~~(num1/num2*1000);
+    
+    // 방법5(동료풀이)
+    // const solution = (num1, num2) => Math.floor(num1 / num2 * 1000);
+    
+    // 방법6(동료풀이)
+    // return Math.trunc(num1 / num2 * 1000);
 }
