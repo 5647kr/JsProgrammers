9f6a113a459e9f0cf9cc8920097e6913c4b23fe9
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-10-31 22:23:21 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,13 @@
+// 방법1
+function solution(numbers) {
+    return numbers.map((num) => num * 2)
+}
+
+// 방법2
+// function solution(numbers) {
+//     var answer = [];
+//     for(let i = 0; i < numbers.length; i++) {
+//       answer.push(numbers[i] * 2)
+//     }
+//     return answer
+// }
