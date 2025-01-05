86b681f61d20152d269be9c9a437106e8f8f5229
K's_Fe <119591236+5647kr@users.noreply.github.com>
2025-01-05 19:39:02 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,4 @@
 function solution(n) {
     var answer = String(n).split("").map(Number);
-    return answer.reduce((acc, cur) => acc + cur);
+    return answer.reduce((acc, cur) => acc + cur, 0);
 }
